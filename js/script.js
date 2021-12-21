c01aa377cc957eaf334fc9c32aff75db5e7e4ab3
SangesAntonio <sanges65@gmail.com>
2021-12-21 17:06:18 +0100

    fixed age price problem

--- js/script.js
+++ js/script.js
@@ -19,30 +19,35 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
 
 //! 1-Chiedere all'utente il numero di chilometri
 const userKilometers = parseInt(prompt('Quanti chilometri vuoi percorrere?', '70'));
-console.log(userKilometers);
+document.getElementById('chilometri').innerText = `Distanza percorsa: ${userKilometers}km`;
+
+console.log(`userKilometers:  ${userKilometers}`);
 
 //! 2-Chiedere all'utente l'età
 const userAge = parseInt(prompt('Quanti anni hai?', '19'));
-console.log(userAge);
+
+document.getElementById('eta').innerText = `La tua età: ${userAge}anni`;
+
+console.log(`userAge:  ${userAge}`);
 
 //! 3-Calcolare prezzo del biglietto
-let ticketPrice;
+let ticketPrice = userKilometers * 0.21 ;
 
-if((userAge > 18) && (userAge < 65)){
-    ticketPrice = userKilometers * 0.21 ;
-}
-console.log(ticketPrice);
+document.getElementById('costo').innerText = `Costo Totale: ${ticketPrice}$`;
 
-//! 3a-calcolare il prezzo del biglietto se l'età è sotto i 18 anni -20%
-let discount18 = (ticketPrice / 100) * 20;
-let discount65 = (ticketPrice / 100) * 40;
-let ticketPriceDiscount;
+//! 3a-calcolare il prezzo del biglietto se l'età richiede uno sconto
 
 //calcolo dello sconto
 if(userAge < 18){
-    ticketPriceDiscount = ticketPrice - discount18;
+    const discountUnder = (ticketPrice / 100) * 20;
+    ticketPrice = ticketPrice - discountUnder;
+    document.getElementById('costo').innerText = `Costo Totale per minorenni: ${ticketPrice}$`;
+
 } else if (userAge > 65){
-    ticketPriceDiscount = ticketPrice - discount65;
+    const discountAbove = (ticketPrice / 100) * 40;
+    ticketPrice = ticketPrice - discountAbove;
+    document.getElementById('costo').innerText = `Costo Totale per anziani: ${ticketPrice}$`;
+
 }
 
-console.log(ticketPriceDiscount);
+console.log(`ticketprice:  ${ticketPrice}`);
